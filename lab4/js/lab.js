58575ec57124173b7bd48c4368b02bd072535e9a
mellymak <mymak@ucsc.edu>
2021-10-14 02:34:51 -0700

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,17 @@
+// Lab 4 : Pseudocoding and Problem-solving.
+// Pseudocoding a webpage process to animate how to make a matcha latte via animations and user input.
+// @link   URL
+// @file   art101 class.
+// @author MelanieMak.
+// @since  10.13.21
+
+//Create a Matcha Latte via Webpage Process
+  // 1. Print the background before each turn
+  // 2. Accept user input
+      // a. User provides input through clicking
+  // 3. Validate user move
+      // a. Clicking through queues each animation
+  // 4. Evaluate possible moves
+      // a. User can choose type of sweetener
+      // b. User can choose type of milk
+  // 5. Print corresponding animation to user’s choice
