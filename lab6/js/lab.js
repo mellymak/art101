b3544e879ed087fcaf5d961c27e63da2d67edf15
mellymak <mymak@ucsc.edu>
2021-10-21 03:25:07 -0700

    updatejs

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -3,8 +3,8 @@
 * Created: October 21, 2021
 */
 
-var myTransport - ['walking', 'bus'];
-var myMainRide - {
+var myTransport = ['walking', 'bus'];
+var myMainRide = {
 	make: 'audi',
 	model: 'a8',
 	color: 'silver',
@@ -17,6 +17,6 @@ var myMainRide - {
 
 
 //output
-document.writeln("Getting around:" + myTransport + "<br>");
+document.writeln("Getting around: " + myTransport + "<br>");
 document.writeln("My Main Ride: <pre>", 
     JSON.stringify(myMainRide, null, '\t'), "</pre>");
