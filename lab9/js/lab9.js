5005516dd84c15ef230393d79e99d3a36e7068ad
mellymak <mymak@ucsc.edu>
2021-11-02 01:44:14 -0700

    Update lab9.js

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -4,11 +4,11 @@
  * License: Public Domain
  */
 
- // Use getElementById() to find your output <div> and assign it to a variable outputEl
+ //find your output <div> and assign it to a variable outputEl
 document.getElementById("output");
  var outputEl = document.getElementById("output");
 
- // Create a new element with document.createElement("p") and assign it to a variable new1El
+ // Create a new element and assign it to a variable new1El
 document.createElement("p")
  var new1El = document.createElement("p")
 
@@ -16,18 +16,18 @@ document.createElement("p")
 new1El.innerHTML = "howdy partner!";
 
  // Create another new element and assign it to a variable new2El
-document.createElement("new")
+document.createElement("button")
  var new2El = document.createElement("button");
 
  // Change the html attribute of new2El to say something else.
 new2El.innerHTML = "<button id=magic>you are so magical!</button>";
 
  // Append both new elements one at a time using appendChild() to your output <div>
-outputEl.appendChild(new1El)
-outputEl.appendChild(new2El)
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
 
  // // Change the css attributes of at least two elements of your page
- new2El.style.color = "white";
- new2El.style.backgroundcolor = "pink";
+ new2El.style.color = "white"
+ new2El.style.backgroundcolor = "pink"
 
- new1El.style.color = "green";
+ new1El.style.color = "green"
