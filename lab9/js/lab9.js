67cb6eaaacb32304b42cb4fdc749a119bf57907b
mellymak <mymak@ucsc.edu>
2021-11-02 02:01:49 -0700

    Update lab9.js

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -4,7 +4,7 @@
  * License: Public Domain
  */
 
- //find your output <div> and assign it to a variable outputEl
+ // find your output <div> and assign it to a variable outputEl
 document.getElementById("output");
  var outputEl = document.getElementById("output");
 
@@ -20,13 +20,13 @@ document.createElement("button");
  var new2El = document.createElement("button");
 
  // Change the html attribute of new2El to say something else.
-new2El.innerHTML = "<button id=magic>you are so magical!</button>";
+new2El.innerHTML = "<button id=button>you are so magical!</button>";
 
  // Append both new elements one at a time using appendChild() to your output <div>
 outputEl.appendChild(new1El);
 outputEl.appendChild(new2El);
 
- // // Change the css attributes of at least two elements of your page
+ // Change the css attributes of at least two elements of your page
  new2El.style.color = "white"
  new2El.style.backgroundcolor = "pink"
 
