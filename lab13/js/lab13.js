92af9053365df3e2492111bffa803beef4dae70c
mellymak <mymak@ucsc.edu>
2021-11-20 20:03:47 -0800

    update13

---
+++ lab13/js/lab13.js
@@ -0,0 +1,28 @@
+/*
+ * Author: Melanie Mak
+ * Created: 11.20.21
+ * License: Public Domain
+ */
+function count() {
+ for (var i=0; i<=200; i++) {
+     var str= "";
+     if (i % 3 == 0) {
+         str += "Fizz";
+     }
+     if (i % 5 == 0) {
+         str += "Buzz";
+     }
+     if (i % 7 == 0) {
+         str += "Boom";
+     }
+     if (str == "") {
+         str = i;
+     }
+     else {
+         str += "!";
+     }
+     console.log(str);
+  }
+ }
+
+$("#output").append("<p>" + str + "</p>");
