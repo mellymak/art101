452b607358ef9aba1737d3291eabd1810d5c1925
mellymak <mymak@ucsc.edu>
2021-11-23 00:39:32 -0800

    update

--- lab13/js/lab13.js
+++ lab13/js/lab13.js
@@ -3,10 +3,13 @@
  * Created: 11.20.21
  * License: Public Domain
  */
+ //create temporary string for output
 result = ""
-
+//create function FizzBuzzBoom with parameter to change
 function FizzBuzzBoom(result) {
+  //loop will run numbers 1-200
  for (var num=0; num<=200; num++) {
+   //create temporary string to sort the numbers
      var str= "";
      if (num % 3 == 0) {
          str += "Fizz";
@@ -22,7 +25,8 @@ function FizzBuzzBoom(result) {
      }
      result += str;
      }
+     //print results into output section
      $("output").html(result);
   }
-
+//Call the FizzBuzzBoom function
 FizzBuzzBoom(result);
