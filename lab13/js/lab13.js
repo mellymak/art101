1b9a966c6d345ac8f397f24e5b67119449f55018
mellymak <mymak@ucsc.edu>
2021-11-23 00:35:27 -0800

    Update lab13.js

--- lab13/js/lab13.js
+++ lab13/js/lab13.js
@@ -3,24 +3,26 @@
  * Created: 11.20.21
  * License: Public Domain
  */
-function FizzBuzzBoom() {
+result = ""
+
+function FizzBuzzBoom(result) {
  for (var num=0; num<=200; num++) {
      var str= "";
      if (num % 3 == 0) {
          str += "Fizz";
-         $("#output").append("<p>" + str + "</p>");
      }
      if (num % 5 == 0) {
          str += "Buzz";
-         $("#output").append("<p>" + str + "</p>");
      }
      if (num % 7 == 0) {
          str += "Boom";
-         $("#output").append("<p>" + str + "</p>");
      }
-     else {
+     if (str.length > 0){
          str += "!</br>";
      }
-     console.log(num.toString() + str);
+     result += str;
+     }
+     $("output").html(result);
   }
-}
+
+FizzBuzzBoom(result);
