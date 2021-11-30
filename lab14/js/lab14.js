060728d06a4eed3fb54fefa951c771ed76144028
mellymak <mymak@ucsc.edu>
2021-11-30 00:57:40 -0800

    lab14

---
+++ lab14/js/lab14.js
@@ -0,0 +1,32 @@
+/*
+ * Author: Melanie Mak
+ * Created: 11.29.21
+ * License: Public Domain
+ */
+ //create temporary string for output
+result = ""
+//create function FizzBuzzBoom with parameter to change
+function FizzBuzzBoom(result) {
+  //loop will run numbers 1-200
+ for (var num=0; num<=200; num++) {
+   //create temporary string to sort the numbers
+     var str= "";
+     if (num % 3 == 0) {
+         str += "Fizz";
+     }
+     if (num % 5 == 0) {
+         str += "Buzz";
+     }
+     if (num % 7 == 0) {
+         str += "Boom";
+     }
+     if (str.length > 0){
+         str += "!</br>";
+     }
+     result += str;
+     }
+     //print results into output section
+     $("output").html(result);
+  }
+//Call the FizzBuzzBoom function
+FizzBuzzBoom(result);
